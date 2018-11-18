44c0efb6f9dddcc5e31dfe05836ef8bca36804b9
Marcelo Medeiros <marcelomedeirosb@gmail.com>
2018-11-18 02:05:26 -0200

    retirados links genericos do menu

--- portal/menu.php
+++ portal/menu.php
@@ -43,8 +43,8 @@ if((isset ($_SESSION['acesso']) == TRUE)){
                 echo (in_array("S", [$_SESSION["ind_aluno"], $_SESSION["ind_professor"]]) ? "<li><a href=\"materias.html\">Materias</a></li>" : "");
                 echo (in_array("S", [$_SESSION["ind_aluno"], $_SESSION["ind_professor"]]) ? "<li><a href=\"notas.html\">Notas</a></li>" : "");
                 ?>
-              <li><a href="generic.html">Generic</a></li>
-              <li><a href="elements.html">Elements</a></li>
+              <!-- <li><a href="generic.html">Generic</a></li>
+              <li><a href="elements.html">Elements</a></li> -->
               <li><a href="logout.php">Sair</a></li>
         </ul>
     </nav>
